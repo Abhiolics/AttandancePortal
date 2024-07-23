7f51e84b52e082e8b6cb677f114d385ee0ba5c53
alokdubey01 <alok73327@gmail.com>
2024-07-23 19:33:00 +0530

    route chnaged

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-//export default BASE_URL = "https://attendence-api-px8b.onrender.com"
+// export const BASE_URL = "http://localhost:8000";
 
-export const BASE_URL = "http://localhost:8000"
+export const BASE_URL = "https://attendence-api-px8b.onrender.com";
