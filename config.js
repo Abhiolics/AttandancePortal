c1cdbd20f8607a3385019255f0358dc32cb7e5f3
alokdubey01 <alok73327@gmail.com>
2024-08-02 20:11:50 +0530

    url

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://localhost:5000";
+// export const BASE_URL = "http://localhost:5000";
 
 // export const BASE_URL = "https://attendence-api-px8b.onrender.com";
 
-// export const BASE_URL = "https://attend.anujdwivedi.in";
+export const BASE_URL = "https://attend.anujdwivedi.in";
